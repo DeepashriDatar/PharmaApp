3747f864152543347add9604cafda8454c2fa36a
DeepashriDatar <deepashri.datar@waiin.com>
2024-06-25 12:00:56 +0530

    set product info page

--- src/components/ProductInfo.js
+++ src/components/ProductInfo.js
@@ -47,17 +47,32 @@ const ProductInfo = () => {
     });
 
   }
+  let defaultImg = "https://dims.apnews.com/dims4/default/e9dd673/2147483647/strip/true/crop/8640x4860+0+450/resize/1440x810!/quality/90/?url=https%3A%2F%2Fassets.apnews.com%2F8e%2Fec%2Fa444ae76cdd64fa9ad5f2c67460a%2F67e4f10f96a042c3969c3797a50e9fa1"
 
   return (
-    <div className="card">
-      <h2>Product Info : {prodData.name}</h2>
+    <div className="row">
+      <h2 style={{ textAlign: "center", color: "red" }}>Product Info : {prodData.name}</h2>
       <br></br>
-      <h3> Description: {prodData.description}</h3>
-      <h3> Current price: {prodData.price}</h3>
+      <div className="row">
+        <div className="col-md-4"></div>
+        <div className="col-md-4 border-single" style={{ border: "solid" }}>
+          <img src={!prodData.image ? defaultImg : prodData.image} className="card-img-top" alt="..."
+            style={{ width: "20rem", height: "20rem" }} />
 
-      <br></br>
-      <button className="btn btn-primary" onClick={() => addToCart(prodData.id)}>Add to Cart</button>
-      <Link className="btn btn-primary" to="/product-details">Product List</Link>
+          <h5> Description: {prodData.description}</h5>
+          <br></br>
+          <h5> Current price: {prodData.price} Rs.</h5>
+
+          <br></br>
+          <br></br>
+          <button className="btn btn-primary" onClick={() => addToCart(prodData.id)}>Add to Cart</button>
+          <Link className="btn btn-primary" to="/product-details" style={{ float: "right" }}>Product List</Link>
+          <br></br>
+          <br></br>
+        </div>
+        <div className="col-md-4"></div>
+        <br></br>
+      </div>
 
     </div>
   )
