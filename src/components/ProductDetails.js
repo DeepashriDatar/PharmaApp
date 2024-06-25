56da0540c725c243155c3ed9ef0673f65795556c
DeepashriDatar <deepashri.datar@waiin.com>
2024-06-25 10:22:05 +0530

    filters added

--- src/components/ProductDetails.js
+++ src/components/ProductDetails.js
@@ -1,7 +1,7 @@
-import React, {useEffect, useState } from 'react';
-import {useNavigate} from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
-const ProductDetails = ({addTocart})=> {
+const ProductDetails = () => {
   const [products, setprodData] = useState([]);
   const navigate = useNavigate();
 
@@ -16,90 +16,101 @@ const ProductDetails = ({addTocart})=> {
   }, [])
 
   const showDetails = (id) => {
-    navigate("/product-info/"+id);
+    navigate("/product-info/" + id);
   }
 
   const addToCart = (id) => {
     var curProd = products.find((element) => element.id === id);
     console.log(curProd);
 
-    fetch("http://localhost:8080/cart",{
+    fetch("http://localhost:8080/cart", {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
       },
-      body: JSON.stringify({id:curProd.id, name:curProd.name, price:curProd.price, description:curProd.description})
-    }).then((res)=>{
+      body: JSON.stringify({ id: curProd.id, name: curProd.name, price: curProd.price, description: curProd.description })
+    }).then((res) => {
       return res.json();
-    }).then((data)=>{
+    }).then((data) => {
       alert("Item added to cart successfully");
-    }).catch((err)=>{
+    }).catch((err) => {
       console.error('Error fetching data: ', err);
     });
   }
   const [filter, setFilter] = useState('');
   const [sort, setSort] = useState('price-asc');
 
-  const filteredProducts = products
-  .filter(product => product.name.toLowerCase().includes(filter.toLowerCase()))
-  .sort((a, b) => (sort === 'price-asc' ? a.price - b.price : b.price - a.price));
+  const addFilter = (value) => {
+    console.log(products);
+    if(value){
+      setFilter(value);
+      const filteredProducts = products
+      .filter(product => product.name.toLowerCase().includes(filter.toLowerCase()))
+      .sort((a, b) => (sort === 'price-asc' ? a.price - b.price : b.price - a.price));
+      setprodData(filteredProducts);
+    } else {
+      setFilter(products);
+    }
+  }
 
+  let defaultImg = "https://dims.apnews.com/dims4/default/e9dd673/2147483647/strip/true/crop/8640x4860+0+450/resize/1440x810!/quality/90/?url=https%3A%2F%2Fassets.apnews.com%2F8e%2Fec%2Fa444ae76cdd64fa9ad5f2c67460a%2F67e4f10f96a042c3969c3797a50e9fa1"
 
-    let defaultImg = "https://dims.apnews.com/dims4/default/e9dd673/2147483647/strip/true/crop/8640x4860+0+450/resize/1440x810!/quality/90/?url=https%3A%2F%2Fassets.apnews.com%2F8e%2Fec%2Fa444ae76cdd64fa9ad5f2c67460a%2F67e4f10f96a042c3969c3797a50e9fa1"
-  
-    return (
-      <div className='container my-3'>
-          <br></br>
-          <h3> Product Details </h3>
+  return (
+    <div className='container my-3'>
+      <br></br>
+      <h3> Product Details </h3>
 
-          <div className="col-md-6">
-          <input
-            type="text"
-            className="form-control"
-            placeholder="Filter by name"
-            value={filter}
-            onChange={(e) => setFilter(e.target.value)}
-          />
-        </div>
-        <div className="col-md-6">
-          <select className="form-control" value={sort} onChange={(e) => setSort(e.target.value)}>
-            <option value="price-asc">Sort by Price: Low to High</option>
-            <option value="price-desc">Sort by Price: High to Low</option>
-          </select>
-        </div>
+      <div className="col-md-6">
+        <input
+          type="text"
+          className="form-control"
+          placeholder="Filter by name"
+          value={filter}
+          onChange={(e) => addFilter(e.target.value)}
+        />
+      </div>
+      <div className="col-md-6">
+        <select className="form-control" value={sort} onChange={(e) => setSort(e.target.value)}>
+          <option value="price-asc">Sort by Price: Low to High</option>
+          <option value="price-desc">Sort by Price: High to Low</option>
+        </select>
+      </div>
 
-        <div className='row'> 
-                       
-          {products.map((element)=>{
-            
-            return(
+      <br></br>
+      <br></br>
+
+      <div className='row'>
+
+        {products.map((element) => {
+
+          return (
             <div className='col-md-4' key={element.id}>
-              <div className="card" style={{width: "18rem" }}>
-                <img src={!element.image ? defaultImg: element.image} className="card-img-top" alt="..." />
-                    <div className="card-body">
-                      
-                    <h5 className="card-title">
-                        {`${element?.name ? element.name.length > 40 ? element.name.slice(0,40) : element.name: ""} (Rs.  ${element.price} )` }                    
-                    </h5> 
-                        
-                      
-                        <p className="card-text"> {element.description}... </p>
-                        <button className="btn btn-primary" onClick={() => {showDetails(element.id)}}>
-                          Details
-                          </button>
+              <div className="card" style={{ width: "18rem" }}>
+                <img src={!element.image ? defaultImg : element.image} className="card-img-top" alt="..." />
+                <div className="card-body">
 
-                        <button className="btn btn-primary" onClick={() => {addToCart(element.id)}}>
-                          Add to Cart
-                        </button>
-                  
-                  </div>
+                  <h5 className="card-title">
+                    {`${element?.name ? element.name.length > 40 ? element.name.slice(0, 40) : element.name : ""} (Rs.  ${element.price} )`}
+                  </h5>
+
+
+                  <p className="card-text"> {element.description}... </p>
+                  <button className="btn btn-primary" onClick={() => { showDetails(element.id) }}>
+                    Details
+                  </button>
+
+                  <button className="btn btn-primary" onClick={() => { addToCart(element.id) }}>
+                    Add to Cart
+                  </button>
+
+                </div>
               </div>
 
             </div>
-            )
-          })} 
-        </div>
+          )
+        })}
       </div>
+    </div>
   );
 };
 
